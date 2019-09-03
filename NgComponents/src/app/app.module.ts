e8292bc1cb0b3421c4eec61eec8a9467122109da
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-09-03 21:54:09 +0530

    Commit#4

--- NgComponents/src/app/app.module.ts
+++ NgComponents/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CockpitComponent } from './cockpit/cockpit.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CockpitComponent
+    CockpitComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
