b00a0a76d724cf8f0e42c092c05033370bc6439a
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-09-04 19:32:13 +0530

    Commit#7

--- NgComponents/src/app/app.module.ts
+++ NgComponents/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CockpitComponent } from './cockpit/cockpit.component';
-import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
