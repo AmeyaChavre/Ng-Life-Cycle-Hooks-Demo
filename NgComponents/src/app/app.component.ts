17c9638c4489897ef9cb885a1e0852cdcfbf6412
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-09-05 18:54:40 +0530

    Commit#64

--- NgComponents/src/app/app.component.ts
+++ NgComponents/src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [{type:'server' ,name:'TestServer', content:'This is a test server'}];
+  
+  onAddServer() {
+    //this.serverElements.push({
+     // type: 'server',
+      //name: this.newServerName,
+      //content: this.newServerContent
+    //});
+  }
+
+  onAddBlueprint() {
+  //  this.serverElements.push({
+    //  type: 'blueprint',
+     // name: this.newServerName,
+      //content: this.newServerContent
+    //});
+  }
 }
