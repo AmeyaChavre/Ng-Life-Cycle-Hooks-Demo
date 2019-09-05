7fb85b8f00807b5a6ddf79d9e709644e096467cb
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-09-05 22:03:35 +0530

    Commit#119

--- NgComponents/src/app/cockpit/cockpit.component.ts
+++ NgComponents/src/app/cockpit/cockpit.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
-  @Output('') serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output('') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   newServerName = '';
   newServerContent = '';
