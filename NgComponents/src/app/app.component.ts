fec079ceb9c428cb3d8842938f95453a229de908
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-09-05 19:19:19 +0530

    Commit#92

--- NgComponents/src/app/app.component.ts
+++ NgComponents/src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent {
     });
   }
 
-  onBlueprintAdded() {
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
     this.serverElements.push({
     type: 'blueprint',
     name: this.newServerName,
