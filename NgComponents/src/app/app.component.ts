87820cd50524069b43491af26ecdd63a2055c711
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-09-05 19:11:08 +0530

    Commit#86

--- NgComponents/src/app/app.component.ts
+++ NgComponents/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   serverElements = [{type:'server' ,name:'TestServer', content:'This is a test server'}];
 
-  onServerAdded(serverData: {serverName: string,}) {
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
     this.serverElements.push({
     type: 'server',
     name: this.newServerName,
