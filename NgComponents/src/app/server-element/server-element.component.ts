aadb5e9e41f544b6e46bb404e223466bd6716d8b
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-09-06 21:24:20 +0530

    Commit183

--- NgComponents/src/app/server-element/server-element.component.ts
+++ NgComponents/src/app/server-element/server-element.component.ts
@@ -10,7 +10,8 @@ export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: {type:string ,name:string, content:string}; // defining an object element 
 
   // using @Input decorator to expose the property to parent compnent in this case app component
-  constructor() { }
+  constructor() { 
+  }
 
   ngOnInit() {
   }
