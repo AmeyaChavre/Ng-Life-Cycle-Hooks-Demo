df50fedfe9e174568dfece3434360c38d2e0863e
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-09-08 10:47:37 +0530

    Commit#261

--- NgComponents/src/app/server-element/server-element.component.ts
+++ NgComponents/src/app/server-element/server-element.component.ts
@@ -48,6 +48,7 @@ OnDestroy
   ngOnInit() {
   	console.log('ngOnInit() called !');
   	console.log('Text Content : ' + this.header.nativeElement.textContent); // the text won't appear
+  	
   }
 
   ngDoCheck(){
